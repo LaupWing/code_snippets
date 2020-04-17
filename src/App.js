36844ddd3534f9136198ad61180f777a9896bbd1
LaupWing <loc_strict@hotmail.com>
2020-04-17 14:54:37 +0200

    Added editor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './App.module.css'
+import Editor from './Editor/Editor'
 
 function App() {
     return (
         <div className={styles.App}>
-    
+            <Editor/>
         </div>
     );
 }
