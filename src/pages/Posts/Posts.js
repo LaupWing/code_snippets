0c23d047565d8aa8059b1a36ecbd95296089f30c
LaupWing <loc_strict@hotmail.com>
2020-04-20 01:51:54 +0200

    nice

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -4,7 +4,16 @@ import UserContext from '../../context/UserContext';
 
 function Posts(props){
     const {data} = useContext(UserContext);
-    console.log(data);
+    if(props.location.pathname.includes('oldest')){
+        console.log('Transform data from oldest first');
+        console.log(data);
+    }else if(props.location.pathname.includes('random')){
+        console.log('Transform data random');
+        console.log(data);
+    }else{
+        console.log('Transform form newest');
+        console.log(data);   
+    }
     return (
         <>
             <h2>{props.title}</h2>
