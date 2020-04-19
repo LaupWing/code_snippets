ba5af2c811986f087fd9fbd1a0ecbe75d79a68ab
LaupWing <loc_strict@hotmail.com>
2020-04-19 13:47:18 +0200

    removed app.js out of the auth state chagned

--- src/index.js
+++ src/index.js
@@ -9,15 +9,17 @@ import './db';
 
 
 firebase.auth().onAuthStateChanged(()=>{
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
+    console.log('Auth has changed');
+    console.log('--This needs some testing eg: testing if it re renders the app');
 });
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 
 serviceWorker.unregister();
