4bc424c8abdaee0a3d84405d89b275782b941c5e
LaupWing <loc_strict@hotmail.com>
2020-04-19 13:11:37 +0200

    rewritten to functional

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,31 +1,27 @@
-import React, {Fragment, Component} from 'react'
+import React, {useState} from 'react'
 import Nav from '../../components/Nav/Nav'
 import SideNav from '../../components/SideNav/SideNav'
 import styles from './Layout.module.css'
 import Modal from '../../components/Modal/Modal'
 
-class Layout extends Component{
-    state={
-        login: false
-    }
-    render(){
-        return(
-            <Fragment>
-                <Nav login={()=>this.setState({login:!this.login})}/>
-                <main className={styles.content}>
-                    <SideNav/>
-                    <section className={styles.Section}>
-                        {this.props.children}
-                    </section>
-                </main>
-                {this.state.login && 
-                    <Modal close={()=>this.setState({login:false})}>
-                        Test
-                    </Modal>
-                }
-            </Fragment>
-        )
-    }
+function Layout(props){
+    const [login, setLogin] = useState(false)
+    return(
+        <>
+            <Nav login={()=>setLogin(!login)}/>
+            <main className={styles.content}>
+                <SideNav/>
+                <section className={styles.Section}>
+                    {props.children}
+                </section>
+            </main>
+            {login && 
+                <Modal close={()=>setLogin(false)}>
+                    Test
+                </Modal>
+            }
+        </>
+    )
 }
 
 export default Layout
--- src/pages/Videos/Videos.js
+++ src/pages/Videos/Videos.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default ()=>(
-    <React.Fragment>
+    <>
         <h2>Videos</h2>
         <p>Ordered the code snippets by videos</p>
-    </React.Fragment>
+    </>
 )
