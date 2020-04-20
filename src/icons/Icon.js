89ad780d2093bc4106da22cab123f3951636c971
LaupWing <loc_strict@hotmail.com>
2020-04-20 14:20:15 +0200

    base styling

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -2,9 +2,9 @@ import React from 'react';
 import icons from './icons';
 
 export default ({skill})=>{
-    const Icon = icons[skill]
+    const Icon = icons[skill];
     return(
         <Icon/>
-    )
+    );
 }
 
--- src/pages/Posts/Post/Post.js
+++ src/pages/Posts/Post/Post.js
@@ -5,7 +5,14 @@ import Icon from '../../../icons/Icon';
 function Post({post}){
     return(
         <div className={styles.Post}>
-            <Icon skill={post.skill}/>
+            <div className={styles.skill}>
+                <Icon skill={post.skill}/>
+                <h3>{post.skill}</h3>
+            </div>
+            <div className={styles.content}>
+                <h3>{post.title}</h3>
+                <p>{post.description}</p>
+            </div>
         </div>
     );
 }
