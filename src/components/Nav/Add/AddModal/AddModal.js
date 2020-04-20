0dc68fd4b92c4d3bab350fc7f86d6fc75429590a
LaupWing <loc_strict@hotmail.com>
2020-04-20 19:36:19 +0200

    added editor

--- src/components/Nav/Add/AddModal/AddModal.js
+++ src/components/Nav/Add/AddModal/AddModal.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './AddModal.module.css';
+import MyEditor from '../../../Editor/Editor';
 
 function AddModal(){
     return (
-        <form>
-            <h2>Add New</h2>
+        <form className={styles.AddModal}>
+            <input name="title" type="text" placeholder="Title of code"></input>
+            <input name="description" type="text" placeholder="Your Description"></input>
+            <MyEditor/>
         </form> 
     );
 }
