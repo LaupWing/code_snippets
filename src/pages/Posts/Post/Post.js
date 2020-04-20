16a0bb1814dd068fa64740d3de896d8616d30bf2
LaupWing <loc_strict@hotmail.com>
2020-04-20 14:29:28 +0200

    fixed created at

--- src/pages/Posts/Post/Post.js
+++ src/pages/Posts/Post/Post.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './Post.module.css';
 import Icon from '../../../icons/Icon';
+import dateFormat from 'dateformat';
 
 function Post({post}){
+    const date = new Date(post.createdAt.seconds*1000);
+    const createdAt = dateFormat(date);
+    
     return(
         <div className={styles.Post}>
             <div className={styles.skill}>
@@ -10,6 +14,7 @@ function Post({post}){
                 <h3>{post.skill}</h3>
             </div>
             <div className={styles.content}>
+                <i>{createdAt}</i>
                 <h3>{post.title}</h3>
                 <p>{post.description}</p>
             </div>
