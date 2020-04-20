ce51a2eb54cd2ecd7e607cb83b87f3bfb9df68e2
LaupWing <loc_strict@hotmail.com>
2020-04-20 12:58:17 +0200

    working logo

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,7 +3,7 @@ import Nav from '../../components/Nav/Nav';
 import SideNav from '../../components/SideNav/SideNav';
 import styles from './Layout.module.css';
 import Modal from '../../components/Modal/Modal';
-import Auth from '../../components/Auth/Auth'
+import Auth from '../../components/Auth/Auth';
 
 function Layout(props){
     const [modal, setModal] = useState(false);
