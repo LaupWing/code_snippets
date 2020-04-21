96dfa36c11e2d360b5454dfbc2d82fc3ec96c1ab
LaupWing <loc_strict@hotmail.com>
2020-04-21 17:43:50 +0200

    aded skill select

--- src/components/Nav/Add/AddModal/AddModal.js
+++ src/components/Nav/Add/AddModal/AddModal.js
@@ -1,10 +1,12 @@
 import React, {useRef} from 'react';
 import styles from './AddModal.module.css';
 import MyEditor from '../../../Editor/Editor';
+import * as icons from '../../../../icons/icons';
 
 function AddModal(){
     let content = null;
     const formEl = useRef(null);
+    const availableIcons = Object.keys(icons.default); 
 
     const handleSubmit = (e)=>{
         e.preventDefault();
@@ -16,9 +18,23 @@ function AddModal(){
     const setContent = (text)=>{
         content = text
     }
+    const options = availableIcons.map(icon=>(
+        <option>{icon}</option>
+    ))
 
     return (
         <form ref={formEl} onSubmit={handleSubmit} className={styles.AddModal}>
+            <div className={styles.skills}>
+                <select>
+                    <option value="" disabled selected>Select Skill</option>
+                    {options}
+                </select>
+                <select>
+                    <option value="" disabled selected>Select Section</option>
+                </select>
+                 or
+                <input type="text" placeholder="New Section"></input>
+            </div>
             <input name="title" type="text" placeholder="Title of code"></input>
             <input name="description" type="text" placeholder="Your Description"></input>
             <MyEditor setContent={setContent}/>
