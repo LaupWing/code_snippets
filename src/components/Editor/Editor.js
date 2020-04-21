9a3ed814cefbedd0fadba40fe4012659b0afed5d
LaupWing <loc_strict@hotmail.com>
2020-04-21 11:27:49 +0200

    moved styling

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils} from 'draft-js';
 import 'draft-js/dist/Draft.css';
+import './Editor.css';
 
 class MyEditor extends React.Component {
     constructor(props) {
