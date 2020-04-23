0267975ce7e68e06bf8989a8e243509d9fc9c1c2
LaupWing <loc_strict@hotmail.com>
2020-04-23 11:18:49 +0200

    lil refactoring

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -8,27 +8,22 @@ import ArrayHelpers from '../../helpers/arrayhelpers';
 function Posts(props){
     const {data} = useContext(DataContext);
     let posts = null;
-    const setPosts = (ordererd)=>{
-        posts = ordererd.map((p, i)=>(<Post key={i} post={p}/>));
-    }
-    if(props.location.pathname.includes('oldest')){
-        const orderByOldest = data.sort((a,b)=> new Date(a.createdAt.seconds) - new Date(b.createdAt.seconds));
-        setPosts(orderByOldest);
-    }else if(props.location.pathname.includes('random')){
-        const randomized = ArrayHelpers.shuffle(data);
-        setPosts(randomized);
-    }else{
-        const orderByRecent = data.sort((a,b)=>new Date(b.createdAt.seconds) - new Date(a.createdAt.seconds));
-        setPosts(orderByRecent);
-    }
     
+    if(props.location.pathname.includes('oldest')){
+        posts = data.sort((a,b)=> new Date(a.createdAt.seconds) - new Date(b.createdAt.seconds));
+    }else if(props.location.pathname.includes('random')){
+        posts = ArrayHelpers.shuffle(data);
+    }else{
+        posts = data.sort((a,b)=>new Date(b.createdAt.seconds) - new Date(a.createdAt.seconds));
+    }
+    const postsEl = posts.map((p, i)=>(<Post key={i} post={p}/>));
 
     return (
         <>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
             <section className={styles.content}>
-                {posts}
+                {postsEl}
             </section>
         </>
     );
