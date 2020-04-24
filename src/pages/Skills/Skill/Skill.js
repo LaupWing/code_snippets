81a112f02c862657434c1e6448476f043c8a330e
LaupWing <loc_strict@hotmail.com>
2020-04-24 20:59:08 +0200

    added a coutner

--- src/pages/Skills/Skill/Skill.js
+++ src/pages/Skills/Skill/Skill.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './Skill.module.css';
 import Icon from '../../../icons/Icon';
 function Skill({skill}){
+    console.log(skill)
     return(
         <div className={styles.Skill}>
             <Icon skill={skill.skill}/>
             <h2>
-                {skill.skill}
+                {skill.skill} <span>{skill.posts.length}</span>
             </h2>
         </div>
     );
