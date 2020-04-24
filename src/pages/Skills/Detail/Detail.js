e30d2eab7e1f9d413291b65d1da69782efb84ec6
LaupWing <loc_strict@hotmail.com>
2020-04-25 01:36:36 +0200

    fixed teh back button

--- src/pages/Skills/Detail/Detail.js
+++ src/pages/Skills/Detail/Detail.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
+import Icon from '../../../icons/Icon';
+import styles from './Detail.module.css';
+import ArrayHelpers from '../../../helpers/Arrayhelpers'
 
 function Detail({data}){
     const {id} = useParams();
     const detail = data.find(s=>s.skill ===id);
-    console.log(detail);
+    const sections = ArrayHelpers.removeDuplicates(detail.posts.map(x=>x.section));
+    const {push} = useHistory();
 
     return (
-        <div>Detail</div>
+        <>
+            <button onClick={()=>push('/skills')} className={styles.backBtn}>Back to Overview</button>
+            <header className={styles['Detail-Header']}>
+                <Icon skill={detail.skill}/>
+                <div className={styles.meta}>
+                    <h2>{detail.skill}</h2>
+                    <p>
+                        Posts<span> {detail.posts.length} </span> 
+                        Sections<span> {sections.length}</span>
+                    </p>
+                </div>
+            </header>
+            Detail
+        </>
     );
 }
 
