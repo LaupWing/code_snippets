7886594d7a12a27a6d9ed1678f778722dc1865c4
LaupWing <loc_strict@hotmail.com>
2020-04-26 12:41:06 +0200

    weird search bug

--- src/components/Nav/Results/Results.js
+++ src/components/Nav/Results/Results.js
@@ -1,18 +1,24 @@
 import React, {useContext} from 'react';
-import DataContext from '../../../context/DataContext'
+import DataContext from '../../../context/DataContext';
+import SearchHelper from '../../../helpers/SearchHelper';
 
 function Search({search}){
     const {data} = useContext(DataContext);
     console.log(data)
-    const cleanup = data.map(d=>{
-        const copy = d;
-        copy.content = d.content.blocks
-            .map(x=>x.text)
-            .filter(x=>x!=='')
-            .join(' ');
-        return copy
-    })
-    console.log(cleanup);
+    const cleanup = data
+        .map(d=>{
+            d.content = d.content.blocks
+                .map(x=>x.text)
+                .filter(x=>x!=='')
+                .join(' ');
+            return {
+                content: d.content,
+                title: d.title,
+                description: d.description
+            }
+        })
+
+    // SearchHelper(cleanup , search)
     return(
         <div></div>
     );
---
+++ src/helpers/SearchHelper.js
@@ -0,0 +1,3 @@
+export default (data, search)=>{
+    console.log(data, search);
+}
