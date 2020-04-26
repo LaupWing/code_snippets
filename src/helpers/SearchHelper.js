5611a18654a5c6da56641ac99376827eec828cf1
LaupWing <loc_strict@hotmail.com>
2020-04-26 14:50:19 +0200

    there is a bug

--- src/helpers/SearchHelper.js
+++ src/helpers/SearchHelper.js
@@ -4,17 +4,23 @@ export default (data, search)=>{
             .split(' ')
             .filter(x=>x!=='');
     }
-    const searching = format(search);
+    const searching = format(search); 
+    const findSimilarity = c=>searching.find(s=>c.includes(s) ? c : null)
 
     const testing =  data.filter(post=>{
-        const {content} = post;
-        const contentMatch =  format(content)
-            .find(c=>{
-                const matching = searching.find(s=>c.includes(s));
-                return matching ? c : null; 
+        const {content, title, description} = post;
+        const contentMatch =  format(content).find(findSimilarity);
+        const titleMatch =  format(title).find(findSimilarity);
+        const descriptionMatch =  format(description).find(findSimilarity);
+        if(contentMatch){
+            console.log({
+                contentMatch,
+                titleMatch,
+                descriptionMatch
             });
-            return contentMatch ? post : null;
-        });
+        }
+        return (contentMatch||descriptionMatch || titleMatch) ? post : null;
+    });
     console.log(testing);
     // console.log(test)
 }
