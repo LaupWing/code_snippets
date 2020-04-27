02c1520ceb2a59d815fed57833ed77fab3699c48
LaupWing <loc_strict@hotmail.com>
2020-04-27 16:37:29 +0200

    shallow every check

--- src/helpers/SearchHelper.js
+++ src/helpers/SearchHelper.js
@@ -4,29 +4,34 @@ export default (data, search)=>{
             .split(' ')
             .filter(x=>x!=='');
     }
-    const searching = format(search); 
-    const findSimilarity = c=>searching.find(s=>c.toLowerCase().includes(s.toLowerCase()) ? c : null)
-    const similarityObj = ({content, title, description})=>({
-        contentMatch : format(content).find(findSimilarity),
-        titleMatch : format(title).find(findSimilarity),
-        descriptionMatch : format(description).find(findSimilarity)
-    });
+    const searching = format(search);
 
+    const checkSimilarity = formatted => searching.every(s=>formatted.find(c=>c.toLowerCase().includes(s.toLowerCase())));
+
+    const similarityObj = ({content, title, description})=>({
+        contentMatch : checkSimilarity(format(content)),
+        titleMatch : checkSimilarity(format(title)),
+        descriptionMatch : checkSimilarity(format(description))
+    });
+    
     const searchedData =  data
-        .filter(post=>{
-            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
-            return (contentMatch||descriptionMatch || titleMatch) ? post : null;
-        })
-        .map(post=>{
-            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
-            const match = {
-                contentMatch, 
-                titleMatch, 
-                descriptionMatch
-            }
-            return {...post, match}
-        });
-    // console.log(testing);
-    // console.log(test)
-    return searchedData
+    .filter(post=>{
+        const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
+        return (contentMatch||descriptionMatch || titleMatch) ? post : null;
+    })
+    .map(post=>{
+        const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
+        const match = {
+            contentMatch, 
+            titleMatch, 
+            descriptionMatch
+        }
+        return {...post, match}
+    });
+    
+    return searchedData;
 }
+
+
+// Some old code i may use it later!!!
+// const findSimilarity = c=>searching.find(s=>c.toLowerCase().includes(s.toLowerCase()) ? c : null);
