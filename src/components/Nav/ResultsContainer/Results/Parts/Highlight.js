7aaebeaeb846ea717fccf8306e504247a26c817e
LaupWing <loc_strict@hotmail.com>
2020-05-08 11:30:21 +0200

    seperated the highlight logic

---
+++ src/components/Nav/ResultsContainer/Results/Parts/Highlight.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+export default (data, search)=>{
+    const splitBySpaces = string => string
+        .split(' ')
+        .filter(x=>x!=='');
+    
+    const searching = splitBySpaces(search);
+    
+    const checkSimilarity = formatted=>{
+        return searching.every(s=>formatted.find(c=>{
+            return c.toLowerCase().includes(s.toLowerCase())
+        }));
+    };
+
+    const similarityObj = ({content, title, description})=>({
+        contentMatch : checkSimilarity(splitBySpaces(content)),
+        titleMatch : checkSimilarity(splitBySpaces(title)),
+        descriptionMatch : checkSimilarity(splitBySpaces(description))
+    });
+    return data
+        .filter(post=>{
+            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
+            return (contentMatch||descriptionMatch || titleMatch) ? post : null;
+        })
+        .map(post=>{
+            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
+            const match = {
+                content:contentMatch, 
+                title:titleMatch, 
+                description:descriptionMatch
+            };
+            return {...post, match};
+        })
+        .map(post=>{
+            for(const key in post.match){
+                if(post.match[key]){
+                    const valueOfKey = post[key];
+                    let start = null;
+                    let end = null;
+                    const highlighted = splitBySpaces(valueOfKey)
+                        .map(c=>{
+                            const simalarWord = searching.find(x=>c.toLowerCase().includes(x.toLowerCase()));
+                            if(simalarWord){
+                                const highlightedLetters = c.split('')
+                                    .map((q,i)=>{
+                                        if(q.toLowerCase()===simalarWord[0].toLowerCase()){
+                                            const isItThisWord = c.substring(i, (simalarWord.length+i)).toLowerCase();
+                                            const currentSearching = simalarWord.toLowerCase();
+                                            if(isItThisWord === currentSearching){
+                                                start = i;
+                                                end = simalarWord.length+i;
+                                            }
+                                        }
+                                        if(i >= start && i < end){
+                                            return <span key={i}>{q}</span>
+                                        }
+                                        return q
+                                    });
+                                
+                                return highlightedLetters;
+                            }
+                            return c;
+                        })
+                        .map(s=>{
+                            if(typeof s === 'string'){
+                                return s+' ';
+                            }else{
+                                return [...s, ' '];
+                            }
+                        });
+                    post.match[key] =  <p key={post.id}>{highlighted}</p>;
+                }
+            }
+            return post;
+        });
+}
--- src/components/Nav/ResultsContainer/Results/Results.js
+++ src/components/Nav/ResultsContainer/Results/Results.js
@@ -1,85 +1,12 @@
 import React from 'react';
 import Result from './Result/Result';
+import highlight from './Parts/Highlight'; 
 // import styles from './Results.module.css'; 
 
 function SearchResults(props){
     const {data, search} = props;
-    const splitBySpaces = string => string
-        .split(' ')
-        .filter(x=>x!=='');
+    const results = highlight(data, search);
     
-    const searching = splitBySpaces(search);
-    
-    const checkSimilarity = formatted=>{
-        return searching.every(s=>formatted.find(c=>{
-            return c.toLowerCase().includes(s.toLowerCase())
-        }));
-    };
-
-    const similarityObj = ({content, title, description})=>({
-        contentMatch : checkSimilarity(splitBySpaces(content)),
-        titleMatch : checkSimilarity(splitBySpaces(title)),
-        descriptionMatch : checkSimilarity(splitBySpaces(description))
-    });
-
-    let results = null;
-    
-    results = data
-        .filter(post=>{
-            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
-            return (contentMatch||descriptionMatch || titleMatch) ? post : null;
-        })
-        .map(post=>{
-            const {contentMatch, titleMatch, descriptionMatch} = similarityObj(post);
-            const match = {
-                 content:contentMatch, 
-                 title:titleMatch, 
-                 description:descriptionMatch
-            };
-            return {...post, match};
-        })
-        .map(post=>{
-            for(const key in post.match){
-                if(post.match[key]){
-                    const valueOfKey = post[key];
-                    let start = null;
-                    let end = null;
-                    const highlighted = splitBySpaces(valueOfKey)
-                        .map(c=>{
-                            const simalarWord = searching.find(x=>c.toLowerCase().includes(x.toLowerCase()));
-                            if(simalarWord){
-                                const highlightedLetters = c.split('')
-                                    .map((q,i)=>{
-                                        if(q.toLowerCase()===simalarWord[0].toLowerCase()){
-                                            const isItThisWord = c.substring(i, (simalarWord.length+i)).toLowerCase();
-                                            const currentSearching = simalarWord.toLowerCase();
-                                            if(isItThisWord === currentSearching){
-                                                start = i;
-                                                end = simalarWord.length+i;
-                                            }
-                                        }
-                                        if(i >= start && i < end){
-                                            return <span key={i}>{q}</span>
-                                        }
-                                        return q
-                                    });
-                                
-                                return highlightedLetters;
-                            }
-                            return c;
-                        })
-                        .map(s=>{
-                            if(typeof s === 'string'){
-                                return s+' ';
-                            }else{
-                                return [...s, ' '];
-                            }
-                        });
-                    post.match[key] =  <p key={post.id}>{highlighted}</p>;
-                }
-            }
-            return post;
-        });
     const resultsElements = results.map(x=><Result key={x.id} post={x}/>);
     return (
         <>{resultsElements}</>
